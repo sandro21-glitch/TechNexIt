607633f67d7ccf5ad9b194307268c595b15c835c
Sandro <vipsandro11@gmail.com>
2023-09-03 19:12:28 +0400

    Implement automatic user login on app load

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import { store } from "./store";
-import { Provider } from "react-redux";
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -15,6 +13,8 @@ import Error from "./ui/Error";
 import Cart from "./pages/Cart";
 import FixedCart from "./ui/FixedCart";
 import UserAuth from "./features/auth/UserAuth";
+import { useDispatch } from "react-redux";
+import { getCurrentUser } from "./features/auth/authSlice";
 
 function App() {
   const [isOpenAuth, setIsOpenAuth] = useState(false);
@@ -26,46 +26,49 @@ function App() {
     },
   });
 
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
+
   return (
-    <Provider store={store}>
-      <FacebookProvider lazy appId="978208760122939">
-        <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools initialIsOpen={false} />
-          <Router>
-            <Navbar setIsOpenAuth={setIsOpenAuth} />
-            <FixedCart />
-            <UserAuth isOpen={isOpenAuth} setIsOpen={setIsOpenAuth} />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/:category/:id" element={<SingleProduct />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="*" element={<Error />} />
-            </Routes>
-          </Router>
-          <Footer />
-          <Toaster
-            position="top-center"
-            gutter={12}
-            containerStyle={{ margin: "8px" }}
-            toastOptions={{
-              success: {
-                duration: 3000,
-              },
-              error: {
-                duration: 5000,
-              },
-              style: {
-                fontSize: "16px",
-                maxWidth: "500px",
-                padding: "16px 24px",
-                backgroundColor: "white",
-                color: "black",
-              },
-            }}
-          />
-        </QueryClientProvider>
-      </FacebookProvider>
-    </Provider>
+    <FacebookProvider lazy appId="978208760122939">
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Router>
+          <Navbar setIsOpenAuth={setIsOpenAuth} />
+          <FixedCart />
+          <UserAuth isOpen={isOpenAuth} setIsOpen={setIsOpenAuth} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:category/:id" element={<SingleProduct />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
+        <Footer />
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "white",
+              color: "black",
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </FacebookProvider>
   );
 }
 
