3975eee97fc034dff3cc1f5675a7f4dc04660d29
Sandro <vipsandro11@gmail.com>
2023-09-04 02:27:41 +0400

    Update Loading component with full screen loading

--- src/ui/Loading.jsx
+++ src/ui/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="lds-ellipsis">
         <div></div>
         <div></div>
