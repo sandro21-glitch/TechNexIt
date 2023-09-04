7996d965b7d0f55b3059b810969503afc12a2cf7
Sandro <vipsandro11@gmail.com>
2023-09-04 16:26:22 +0400

    change error page layout

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,5 +1,19 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const Error = () => {
-  return <div className="min-h-screen">Page Not Fount, Go back</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="font-rexFontBold min-h-[70vh] flex justify-center flex-col items-center">
+      <p className=" text-[10rem] mb-0">404</p>
+      <p className="mb-5">მოთხოვნილი გვერდი ვერ მოიძებნა!</p>
+      <Link
+        to={navigate(-1)}
+        className="border border-darkBlue p-2 bg-darkBlue text-white hover:text-darkBlue hover:bg-transparent"
+      >
+        მთავარზე დაბრუნება
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
