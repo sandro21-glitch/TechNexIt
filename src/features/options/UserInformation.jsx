842eecc9872ff65800b242303261f4f8f91cfa1c
Sandro <vipsandro11@gmail.com>
2023-09-06 00:35:31 +0400

    Implemented User Profile Management

---
+++ src/features/options/UserInformation.jsx
@@ -0,0 +1,13 @@
+import UserFormItems from "./UserFormItems";
+
+const UserInformation = () => {
+  return (
+    <div>
+      <h3>პირადი ინფორმაცია</h3>
+      <hr />
+      <UserFormItems />
+    </div>
+  );
+};
+
+export default UserInformation;
---
+++ src/features/options/UserInformationForm.jsx
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+
+const UserInformationForm = ({ fields }) => {
+  const { user } = useSelector((state) => state.auth);
+  return (
+    <form className="my-5">
+      {fields.map((field, index) => (
+        <div key={index} className="flex flex-col mb-5">
+          <label htmlFor={field.id}>{field.label}</label>
+          <input
+            type={field.type}
+            id={field.id}
+            disabled={field.id === "email"}
+            className="border border-greyBorder p-2 outline-none focus:border-gray-600"
+            defaultValue={
+              field.id === "email"
+                ? user?.user.email
+                : field.id === "name"
+                ? user?.user.user_metadata.name
+                : ""
+            }
+          />
+        </div>
+      ))}
+      <button
+        type="submit"
+        className="p-2 bg-darkBlue text-white border border-darkBlue hover:text-darkBlue hover:bg-transparent transition-colors ease-in duration-100"
+      >
+        მონაცემების შენახვა
+      </button>
+    </form>
+  );
+};
+
+export default UserInformationForm;
