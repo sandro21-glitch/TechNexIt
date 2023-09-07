5b525024025305ca149123ab09fd9326cff8e5ec
Sandro <vipsandro11@gmail.com>
2023-09-07 19:51:23 +0400

    Remove unnecessary <div> wrapping the <form> element in Register component

--- src/features/auth/Register.jsx
+++ src/features/auth/Register.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
 import { signUpUser } from "./authSlice";
+import { useDispatch } from "react-redux";
 
 const Register = ({ setIsOpen }) => {
   const [email, setEmail] = useState("vipsandro11@gmail.com");
   const [password, setPassword] = useState("21Panda1");
-  const [name, setName] = useState("Sandro");
-
+  const [name, setName] = useState("Sandro21");
   const dispatch = useDispatch();
 
   const handleSignUp = (e) => {
@@ -16,54 +15,52 @@ const Register = ({ setIsOpen }) => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSignUp}>
-        <div className="space-y-3 mt-5">
-          <div className=" flex flex-col">
-            <label htmlFor="email" className="mb-1">
-              სახელი
-            </label>
-            <input
-              type="name"
-              name="name"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="border p-2 outline-none focus:border-gray-700 border-gray-500"
-            />
-          </div>
-          <div className=" flex flex-col">
-            <label htmlFor="email" className="mb-1">
-              ელ. ფოსტა
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="border p-2 outline-none focus:border-gray-700 border-gray-500"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="password" className="mb-1">
-              პაროლი
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="border p-2 outline-none focus:border-gray-700 border-gray-500"
-            />
-          </div>
+    <form onSubmit={handleSignUp}>
+      <div className="space-y-3 mt-5">
+        <div className=" flex flex-col">
+          <label htmlFor="email" className="mb-1">
+            სახელი
+          </label>
+          <input
+            type="name"
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border p-2 outline-none focus:border-gray-700 border-gray-500"
+          />
         </div>
-        <button type="submit" className="bg-yellow-400 p-2 mt-5 text-white">
-          რეგისტრაცია
-        </button>
-      </form>
-    </div>
+        <div className=" flex flex-col">
+          <label htmlFor="email" className="mb-1">
+            ელ. ფოსტა
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border p-2 outline-none focus:border-gray-700 border-gray-500"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="password" className="mb-1">
+            პაროლი
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border p-2 outline-none focus:border-gray-700 border-gray-500"
+          />
+        </div>
+      </div>
+      <button type="submit" className="bg-yellow-400 p-2 mt-5 text-white">
+        რეგისტრაცია
+      </button>
+    </form>
   );
 };
 
