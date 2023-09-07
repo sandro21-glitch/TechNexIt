a810b7f12d088677587dd2404fc62e36f55abd02
Sandro <vipsandro11@gmail.com>
2023-09-07 19:50:34 +0400

    initial commit

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice";
 import authReducer from "./features/auth/authSlice";
-
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
