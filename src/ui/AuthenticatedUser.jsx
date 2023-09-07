122b2271088761160f2e72671abf2fb71fd6544f
Sandro <vipsandro11@gmail.com>
2023-09-07 19:53:42 +0400

    Removed unnecessary imports and simplified the rendering logic of the AuthenticatedUser component.

--- src/ui/AuthenticatedUser.jsx
+++ src/ui/AuthenticatedUser.jsx
@@ -1,10 +1,8 @@
-// import { useUser } from "../hooks/useUser";
 import { BiSolidUser } from "react-icons/bi";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { FiLogOut } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
 import UserOptions from "../features/auth/userOptions";
-import SmallLoader from "../ui/SmallLoader";
 import { toast } from "react-hot-toast";
 import { userSignOut } from "../features/auth/authSlice";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
@@ -14,6 +12,21 @@ const AuthenticatedUser = ({ setIsOpenAuth }) => {
   const { isLoading } = useSelector((store) => store.auth);
   const { error } = useSelector((store) => store.auth);
   const dispatch = useDispatch();
+  if (!user) {
+    return (
+      <ul className="flex items-end gap-5 md:mt-2 justify-between lg:justify-normal">
+        <li
+          onClick={() => setIsOpenAuth(true)}
+          className="flex-center gap-2 cursor-pointer text-[1rem] text-black font-medium hover:text-darkBlue transition-colors ease-in duration-150"
+        >
+          <p className="mb-0">
+            <BiSolidUser />
+          </p>
+          <p>ავტორიზაცია</p>
+        </li>
+      </ul>
+    );
+  }
   if (isLoading) {
     return (
       <SkeletonTheme baseColor="#ffffff" highlightColor="rgb(0, 102, 102)">
@@ -29,23 +42,12 @@ const AuthenticatedUser = ({ setIsOpenAuth }) => {
 
   return (
     <ul className="flex items-end gap-5 md:mt-2 justify-between lg:justify-normal">
-      {!user && (
-        <li
-          onClick={() => setIsOpenAuth(true)}
-          className="flex-center gap-2 cursor-pointer text-[1rem] text-black font-medium hover:text-darkBlue transition-colors ease-in duration-150"
-        >
-          <p className="mb-0">
-            <BiSolidUser />
-          </p>
-          <p>ავტორიზაცია</p>
-        </li>
-      )}
       {user && (
         <li className="group relative flex items-center gap-1 cursor-pointer text-[1rem] text-black font-medium hover:text-darkBlue transition-colors ease-in duration-150">
           <p className="mb-0">
             <BiSolidUser />
           </p>
-          <p>{user.user.user_metadata.name}</p>
+          <p>{user?.user.user_metadata.name}</p>
           <UserOptions />
           <p className="text-xl">
             <IoMdArrowDropdown />
@@ -53,7 +55,7 @@ const AuthenticatedUser = ({ setIsOpenAuth }) => {
         </li>
       )}
 
-      {user && user.user.role === "authenticated" && (
+      {user && user?.user.role === "authenticated" && (
         <li className="flex-center gap-2 cursor-pointer text-[1rem] text-black font-medium hover:text-darkBlue transition-colors ease-in duration-150">
           <p className="font-bold text-black">
             <FiLogOut className="font-extrabold text-black" />
