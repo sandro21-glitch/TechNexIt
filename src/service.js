ae5f9131cb664a4e6e8ff4aea61d06a4c453b328
Sandro <vipsandro11@gmail.com>
2023-09-08 13:44:32 +0400

    Fix environment variable access in code

--- src/service.js
+++ src/service.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_REACT_APP_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
