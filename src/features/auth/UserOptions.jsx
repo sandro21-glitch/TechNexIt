c2c37c29329e0ed28900f57df2323775a86798c1
Sandro <vipsandro11@gmail.com>
2023-09-09 13:08:22 +0400

    Add navigation links to UserOptions component

--- src/features/auth/UserOptions.jsx
+++ src/features/auth/UserOptions.jsx
@@ -1,22 +1,31 @@
 import { FaAngleDoubleRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const UserOptions = () => {
   return (
     <ul className="group-hover:block py-3 hidden absolute min-w-[13rem] h-auto border bg-white text-black border-gray-500 top-full left-0 z-[9999]">
-      <li className="text-[.9rem] flex items-center  gap-1 py-2 pl-2 hover:bg-gray-400">
-        <FaAngleDoubleRight />
-        <span className="mb-0">პირადი ინფორმაცია</span>
+      <li>
+        <Link to='/user' className='text-[.9rem] flex items-center  gap-1 py-2 pl-2 hover:bg-gray-400'>
+          <FaAngleDoubleRight />
+          <span className="mb-0">პირადი ინფორმაცია</span>
+        </Link>
       </li>
-      <li className="text-[.9rem] flex items-center  gap-1 py-2 pl-2 hover:bg-gray-400">
-        <FaAngleDoubleRight />
-        <span className="mb-0">პაროლის შეცვლა</span>
+      <li>
+        <Link className='text-[.9rem] flex items-center  gap-1 py-2 pl-2 hover:bg-gray-400'>
+          <FaAngleDoubleRight />
+          <span className="mb-0">პაროლის შეცვლა</span>
+        </Link>
       </li>
-      <li className="text-[.9rem] flex items-center gap-1 py-2 pl-2 hover:bg-gray-400">
-        <FaAngleDoubleRight />
-        <span className="mb-0">კალათა</span>
+      <li>
+        <Link className='text-[.9rem] flex items-center  gap-1 py-2 pl-2 hover:bg-gray-400'>
+          <FaAngleDoubleRight />
+          <span className="mb-0">კალათა</span>
+        </Link>
       </li>
-      <li className="text-[.9rem] flex items-center gap-1 py-2 pl-2 hover:bg-gray-400">
-        <FaAngleDoubleRight />
-        <span className="mb-0">შეკვეთები</span>
+      <li>
+        <Link className='text-[.9rem] flex items-center  gap-1 py-2 pl-2 hover:bg-gray-400'>
+          <FaAngleDoubleRight />
+          <span className="mb-0">შეკვეთები</span>
+        </Link>
       </li>
     </ul>
   );
