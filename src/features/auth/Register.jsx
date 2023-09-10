43fa914bb9ac33a27bc724483f950ce527a94732
Sandro <vipsandro11@gmail.com>
2023-09-10 22:09:59 +0400

    feat: Add user registration form validation

--- src/features/auth/Register.jsx
+++ src/features/auth/Register.jsx
@@ -1,15 +1,46 @@
 import { useState } from "react";
 import { signUpUser } from "./authSlice";
 import { useDispatch } from "react-redux";
+import toast from "react-hot-toast";
+import { isValidEmail } from "../../utils/emailValid";
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+import { useQuery } from "react-query";
+import { findUserEmail } from "../../services/apiUser";
 
 const Register = ({ setIsOpen }) => {
-  const [email, setEmail] = useState("vipsandro11@gmail.com");
-  const [password, setPassword] = useState("21Panda1");
-  const [name, setName] = useState("Sandro21");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [showPass, setShowPass] = useState(false);
   const dispatch = useDispatch();
+  
+  // Use React Query to check if the email already exists in the database
+  const { data: checkEmail } = useQuery({
+    queryKey: ["users", email],
+    queryFn: () => findUserEmail(email),
+  });
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
+    if (!email || !password || !name) {
+      toast.error("Ყველა ველის შევსება აუცილებელია.");
+      return;
+    }
+    if (checkEmail) {
+      toast.error(
+        "ეს ელფოსტის მისამართი უკვე დაკავშირებულია არსებულ ანგარიშთან. გთხოვთ, გამოიყენოთ სხვა ელფოსტის მისამართი ან სცადოთ შესვლა, თუ ეს თქვენი ანგარიშია"
+      );
+      return;
+    }
+    if (!isValidEmail(email)) {
+      toast.error("ელფოსტის მისამართი არასწორია.");
+      return;
+    }
+
+    if (password.length < 6) {
+      toast.error("პაროლი უნდა იყოს მინიმუმ 8 სიმბოლო.");
+      return;
+    }
     setIsOpen((prevState) => !prevState);
     dispatch(signUpUser({ email, password, name }));
   };
@@ -18,16 +49,17 @@ const Register = ({ setIsOpen }) => {
     <form onSubmit={handleSignUp}>
       <div className="space-y-3 mt-5">
         <div className=" flex flex-col">
-          <label htmlFor="email" className="mb-1">
+          <label htmlFor="name" className="mb-1">
             სახელი
           </label>
           <input
             type="name"
             name="name"
             id="name"
+            maxLength={20}
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className="border p-2 outline-none focus:border-gray-700 border-gray-500"
+            className="border-gray-500 border p-2 outline-none focus:border-gray-700"
           />
         </div>
         <div className=" flex flex-col">
@@ -40,21 +72,27 @@ const Register = ({ setIsOpen }) => {
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="border p-2 outline-none focus:border-gray-700 border-gray-500"
+            className="border-gray-500 border p-2 outline-none focus:border-gray-700"
           />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col relative">
           <label htmlFor="password" className="mb-1">
             პაროლი
           </label>
           <input
-            type="password"
+            type={showPass ? "text" : "password"}
             name="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="border p-2 outline-none focus:border-gray-700 border-gray-500"
+            className="border-gray-500 border p-2 outline-none focus:border-gray-700 "
           />
+          <div
+            onClick={() => setShowPass((prevState) => !prevState)}
+            className="absolute top-1/2 mt-[.1rem] right-3 text-[1.5rem] cursor-pointer"
+          >
+            {showPass ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+          </div>
         </div>
       </div>
       <button type="submit" className="bg-yellow-400 p-2 mt-5 text-white">
