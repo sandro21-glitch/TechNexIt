34d29817295500ffb51279234f942f18542734a6
Sandro <vipsandro11@gmail.com>
2023-09-12 19:20:36 +0400

    refactor: Modify the ProtectedRoute component

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -2,16 +2,23 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "react-loading-skeleton/dist/skeleton.css";
 import Loading from "./Loading";
+import { useLayoutEffect } from "react"; // Import useLayoutEffect
+
 const ProtectedRoute = ({ children }) => {
   const { user, isLoading } = useSelector((store) => store.auth);
   const navigate = useNavigate();
+  useLayoutEffect(() => { // Use useLayoutEffect
+    if (user?.user.role !== "authenticated") {
+      navigate("/");
+    }
+  }, [user?.user.role, navigate]);
 
   if (isLoading) {
     return <Loading />;
   }
-  return user?.user.role === "authenticated" && !isLoading
-    ? children
-    : navigate("/");
+
+
+  return user?.user.role === "authenticated" ? children : null;
 };
 
 export default ProtectedRoute;
