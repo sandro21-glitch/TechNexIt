b11d9d6bd2848ecc370bced2bc55017600a6d5fd
Sandro <vipsandro11@gmail.com>
2023-09-12 18:26:48 +0400

    feat: Simplify ProtectedRoute logic and improve user feedback

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,20 +1,17 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import Loading from "./Loading";
 const ProtectedRoute = ({ children }) => {
   const { user, isLoading } = useSelector((store) => store.auth);
   const navigate = useNavigate();
-  useEffect(() => {
-    if (!user?.user.role && !isLoading) {
-      return navigate("/", { replace: true });
-    }
-  }, [user?.user.role, navigate, isLoading]);
+
   if (isLoading) {
-    return <Skeleton />;
+    return <Loading />;
   }
-  return user?.user.role === "authenticated" ? children : null;
+  return user?.user.role === "authenticated" && !isLoading
+    ? children
+    : navigate("/");
 };
 
 export default ProtectedRoute;
