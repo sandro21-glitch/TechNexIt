882050a768c49f1a1885036effa8412448a0fdc6
Sandro <vipsandro11@gmail.com>
2023-09-13 16:24:05 +0400

    feat: Add functionality to PlaceOrderBtn component

--- src/features/cart/PlaceOrderBtn.jsx
+++ src/features/cart/PlaceOrderBtn.jsx
@@ -1,6 +1,18 @@
+import { toast } from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const PlaceOrderBtn = () => {
+  const { user } = useSelector((store) => store.auth);
+  const navigate = useNavigate();
+  const handleOrder = () => {
+    user
+      ? navigate("/order/OrderPrepare")
+      : toast.error("გაიარეთ ავტორიზაცია შესაკვეთად");
+  };
   return (
     <button
+      onClick={() => handleOrder()}
       className="
       border border-lightBlue bg-lightBlue text-white 
       px-2 py-1 font-rexFontLight hover:bg-transparent hover:text-black 
