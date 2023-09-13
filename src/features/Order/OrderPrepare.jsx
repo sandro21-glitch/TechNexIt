987846c8e836d82986c3fa771f137c4c7828c23d
Sandro <vipsandro11@gmail.com>
2023-09-13 23:08:10 +0400

    feat: Create TermsCheckbox component and use it in OrderPrepare

--- src/features/Order/OrderPrepare.jsx
+++ src/features/Order/OrderPrepare.jsx
@@ -7,7 +7,6 @@ import { useState } from "react";
 import PlaceOrderBtn from "./PlaceOrderBtn";
 
 const OrderPrepare = () => {
-  const [activeCheckbox, setActiveCheckbox] = useState(false);
   return (
     <div className="w-full max-w-[50rem] border-2 border-greyBorder p-5">
       <h2 className="font-rexFontBold text-[1.7rem]">შეკვეთის დეტალები</h2>
@@ -17,16 +16,7 @@ const OrderPrepare = () => {
         <DeliveryMethod />
         <PaymentMethod />
         <OrderSummary />
-        <div className="font-rexFontLight flex items-center gap-2 mb-7">
-          <input type="checkbox" className="w-4 h-4" checked={activeCheckbox} />
-          <p
-            className="text-black cursor-pointer text-lg"
-            onClick={() => setActiveCheckbox((prevState) => !prevState)}
-          >
-            გავეცანი და ვეთანხმები{" "}
-            <Link className="text-darkBlue">საგარანტიო პირობებს</Link>
-          </p>
-        </div>
+        <TermsCheckbox />
         <PlaceOrderBtn />
       </form>
     </div>
---
+++ src/features/Order/TermsCheckbox .jsx
@@ -0,0 +1,23 @@
+const TermsCheckbox = () => {
+  const [activeCheckbox, setActiveCheckbox] = useState(false);
+
+  return (
+    <div className="font-rexFontLight flex items-center gap-2 mb-7">
+      <input
+        type="checkbox"
+        onClick={() => setActiveCheckbox((prevState) => !prevState)}
+        className="w-4 h-4 cursor-pointer"
+        checked={activeCheckbox}
+      />
+      <p
+        className="text-black cursor-pointer text-lg"
+        onClick={() => setActiveCheckbox((prevState) => !prevState)}
+      >
+        გავეცანი და ვეთანხმები{" "}
+        <Link className="text-darkBlue">საგარანტიო პირობებს</Link>
+      </p>
+    </div>
+  );
+};
+
+export default TermsCheckbox;
