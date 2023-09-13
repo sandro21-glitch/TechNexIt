1224182d86e9a17e21856c3997fe78ddacb39bde
Sandro <vipsandro11@gmail.com>
2023-09-13 16:28:50 +0400

    style: Update SinglePageHeader for conditional rendering

--- src/ui/SinglePageHeader.jsx
+++ src/ui/SinglePageHeader.jsx
@@ -3,24 +3,33 @@ import { AiFillHome } from "react-icons/ai";
 import { Link } from "react-router-dom";
 const SinglePageHeader = ({ category, name }) => {
   return (
-    <div className="border-4 border-double border-greyBorder py-2 mb-14">
-      <div className="section-center flex items-center flex-wrap justify-between md:justify-normal gap-5 ">
-        <Link to="/" className="cursor-pointer text-darkBlue text-xl">
-          <AiFillHome />
-        </Link>
-        <span className="text-xl">
-          <BiSolidRightArrowAlt />
-        </span>
-        <p className="capitalize font-rexFontLight">{category}</p>
-        {name ? (
+    <div className="border-4 border-double border-greyBorder py-4 mb-14">
+      {!name && !category ? (
+        ""
+      ) : (
+        <div className="section-center flex items-center flex-wrap justify-between md:justify-normal gap-5 ">
+          <Link to="/" className="cursor-pointer text-darkBlue text-xl">
+            <AiFillHome />
+          </Link>
           <span className="text-xl">
             <BiSolidRightArrowAlt />
           </span>
-        ) : (
-          ""
-        )}
-        <p>{name ? name : null}</p>
-      </div>
+          {category ? (
+            <p className="capitalize font-rexFontLight">{category}</p>
+          ) : (
+            ""
+          )}
+
+          {name ? (
+            <span className="text-xl">
+              <BiSolidRightArrowAlt />
+            </span>
+          ) : (
+            ""
+          )}
+          <p>{name ? name : null}</p>
+        </div>
+      )}
     </div>
   );
 };
