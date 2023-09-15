c22d9a119299f312d6dffb7b7c0fa15ac48b0641
Sandro <vipsandro11@gmail.com>
2023-09-15 16:30:26 +0400

    Create apiOrder.js for handling user order control

---
+++ src/services/apiOrders.js
@@ -0,0 +1,20 @@
+import supabase from "../service";
+const currentDate = new Date();
+const formattedDate = currentDate.toISOString();
+
+export const insertUserOrder = async (userId, userOrder) => {
+  const { error } = await supabase.from("orders").insert({
+    user_id: userId,
+    user_name: userOrder.name,
+    user_number: userOrder.number,
+    product_price: userOrder.price,
+    delivery_method: userOrder.delivery,
+    payment_method: userOrder.payment,
+    bank: userOrder.bank,
+    order_date: formattedDate,
+  });
+  if (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
