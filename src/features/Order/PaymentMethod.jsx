330274709a4623847ad10a415cb0763585e7adb7
Sandro <vipsandro11@gmail.com>
2023-09-15 16:33:30 +0400

    refactor: support dynamic payment method selection

--- src/features/Order/PaymentMethod.jsx
+++ src/features/Order/PaymentMethod.jsx
@@ -1,34 +1,58 @@
-import { useState } from "react";
+import { bankList } from "../../constants/bankList";
 
-const PaymentMethod = () => {
-  const [ctiveCheckbox, setActiveCheckbox] = useState(false);
+const PaymentMethod = ({ setPaymentMethod, paymentMethod }) => {
+  const handleSelectType = (e) => {
+    const selectedType = e.target.value;
+
+    const updatedPaymentMethod =
+      selectedType === "გადახდა პლასტიკური ბარათი"
+        ? { type: selectedType, bank: "საქართველოს ბანკი" }
+        : { type: selectedType };
+
+    setPaymentMethod(updatedPaymentMethod);
+  };
+  const handleSelectBank = (name) => {
+    setPaymentMethod((prevState) => {
+      return { ...prevState, bank: name };
+    });
+  };
+
   return (
     <div className="mb-7">
       <h6 className="mb-1">გადახდის მეთოდი</h6>
-      <select className="mb-3 border border-greyBorder p-2 focus:outline-none w-full cursor-pointer">
-        <option value="">
+      <select
+        onChange={handleSelectType}
+        className="mb-3 border border-greyBorder p-2 focus:outline-none w-full cursor-pointer"
+      >
+        <option value="გადახდა პლასტიკური ბარათით">
           გადახდა პლასტიკური ბარათის მეშვეობით (Visa & Mastercard)
         </option>
-        <option value="">შეძენა ონლაინ განვადების მეშვეობით</option>
+        <option value="ონლაინ განვადების მეშვეობით">შეძენა ონლაინ განვადების მეშვეობით</option>
       </select>
       <div>
-        <div
-          onClick={() => setActiveCheckbox((prevState) => !prevState)}
-          className="relative max-w-[17rem] p-2 border border-greyBorder hover:border-gray-700 cursor-pointer"
-        >
-          <img
-            src="https://itworks.ge/images/payment/card-bog-logo.svg"
-            alt="bog logo"
-            className="w-full "
-          />
-          <div className="absolute right-2 bottom-0">
-            <input
-              type="checkbox"
-              checked={ctiveCheckbox}
-              className="w-4 h-4 cursor-pointer"
+        {paymentMethod.type === "გადახდა პლასტიკური ბარათი" ? (
+          <div className="relative max-w-[17rem] min-h-[4rem] p-2 border border-greyBorder hover:border-gray-700 cursor-pointer">
+            <img
+              src="https://itworks.ge/images/payment/card-bog-logo.svg"
+              alt="bog logo"
+              className="w-full"
             />
           </div>
-        </div>
+        ) : (
+          <ul className="flex flex-col md:flex-row md:items-center items-start gap-5">
+            {bankList.map((bank) => {
+              return (
+                <li
+                  onClick={() => handleSelectBank(bank.name)}
+                  key={bank.id}
+                  className="relative w-full md:max-w-[17rem] min-h-[4rem] p-2 border border-greyBorder hover:border-gray-700 cursor-pointer"
+                >
+                  <img src={bank.img} alt="bog logo" className="w-full " />
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </div>
   );
