5601c387c6ddac247267baff8ba2d14f05d8020a
Sandro <vipsandro11@gmail.com>
2023-09-15 16:29:48 +0400

    Create bankList.js for storing bank information

---
+++ src/constants/BankList.js
@@ -0,0 +1,17 @@
+export const bankList = [
+  {
+    id: 1,
+    img: "https://itworks.ge/images/payment/installment-bog-logo.svg",
+    name:'bog'
+  },
+  {
+    id: 2,
+    img: "https://itworks.ge/images/payment/installment-tbc-logo.svg",
+    name:'tbc'
+  },
+  {
+    id: 3,
+    img: "https://itworks.ge/images/payment/installment-credo-logo.svg",
+    name:'credo'
+  },
+];
