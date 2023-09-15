ec1e4d6697ef83d3b570e5416c0c8a0166b0e9e9
Sandro <vipsandro11@gmail.com>
2023-09-15 16:31:44 +0400

    refactor: manage selected delivery method

--- src/features/Order/DeliveryMethod.jsx
+++ src/features/Order/DeliveryMethod.jsx
@@ -1,13 +1,22 @@
-const DeliveryMethod = () => {
+const DeliveryMethod = ({ setDelieveryMethod }) => {
+  const handleSelectMethod = (e) => {
+    setDelieveryMethod(e.target.value);
+  };
+
   return (
     <div className="mb-7">
       <h6 className="mb-1">მიწოდების მეთოდი</h6>
-      <select className=" border border-greyBorder p-2 focus:outline-none w-full cursor-pointer">
-        <option disabled selected value="">
+      <select
+        onChange={handleSelectMethod}
+        className=" border border-greyBorder p-2 focus:outline-none w-full cursor-pointer"
+      >
+        <option disabled  defaultValue="">
           გთხოვთ აირიჩიოთ თქვენთვის სასურველი მიწოდების მეთოდი
         </option>
-        <option value="">მაღაზიიდან გატანა</option>
-        <option value="">ადგილზე მიწოდება მთელი საქართველოს მასშტაბით</option>
+        <option value="მაღაზიიდან გატანა">მაღაზიიდან გატანა</option>
+        <option value="ადგილზე მიწოდება მთელი საქართველოს მასშტაბით">
+          ადგილზე მიწოდება მთელი საქართველოს მასშტაბით
+        </option>
       </select>
     </div>
   );
