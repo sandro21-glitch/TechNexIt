25d5773e70d6c7d6beed84597c9cae0a1c16e4b4
Sandro <vipsandro11@gmail.com>
2023-09-15 16:34:02 +0400

    refactor: accept and update checkbox state via props

--- src/features/Order/TermsCheckbox .jsx
+++ src/features/Order/TermsCheckbox .jsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const TermsCheckbox = () => {
-  const [activeCheckbox, setActiveCheckbox] = useState(false);
-
+const TermsCheckbox = ({ termsCheckbox, setTermsCheckbox }) => {
   return (
     <div className="font-rexFontLight flex items-center gap-2 mb-7">
       <input
         type="checkbox"
-        onClick={() => setActiveCheckbox((prevState) => !prevState)}
+        onChange={() => setTermsCheckbox((prevState) => !prevState)}
         className="w-4 h-4 cursor-pointer"
-        checked={activeCheckbox}
+        checked={termsCheckbox}
       />
       <p
         className="text-black cursor-pointer text-lg"
-        onClick={() => setActiveCheckbox((prevState) => !prevState)}
+        onChange={() => setTermsCheckbox((prevState) => !prevState)}
       >
         გავეცანი და ვეთანხმები{" "}
         <Link className="text-darkBlue">საგარანტიო პირობებს</Link>
