b34adae4ad2c1bfab3c2e4dc4b32eed9799b64ce
Sandro <vipsandro11@gmail.com>
2023-09-17 13:09:30 +0400

    feat: Add order options buttons

--- src/features/Order/PreparedOrders/OrderPaginate.jsx
+++ src/features/Order/PreparedOrders/OrderPaginate.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import OrderInfo from "./OrderInfo";
+import OrderOptions from "./orderOptions/OrderOptions";
 
 const OrderPaginate = ({ userOrders }) => {
   return (
     <div>
       {userOrders &&
         userOrders.map((order) => (
-          <OrderInfo order={order} key={order.order_id} />
+          <div>
+            <OrderInfo order={order} key={order.order_id} />
+            <OrderOptions order={order} />
+          </div>
         ))}
     </div>
   );
