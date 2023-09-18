f6b6c84538b37b339b02d7d71f5224bf2c18437b
Sandro <vipsandro11@gmail.com>
2023-09-18 12:41:34 +0400

    feat: Refactor RemoveOrder component to include delete functionality

--- src/features/Order/PreparedOrders/orderOptions/RemoveOrder.jsx
+++ src/features/Order/PreparedOrders/orderOptions/RemoveOrder.jsx
@@ -1,9 +1,30 @@
 import { AiFillDelete } from "react-icons/ai";
-const RemoveOrder = () => {
+import { useMutation, useQueryClient } from "react-query";
+import { removeOrder } from "../../../../services/apiOrders";
+import toast from "react-hot-toast";
+
+const RemoveOrder = ({ orderId }) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: remove } = useMutation({
+    mutationKey: ["removeOrder"],
+    mutationFn: () => removeOrder(orderId),
+    onSuccess: () => {
+      queryClient.invalidateQueries("getOrders");
+      toast.success("შეკვეთა წაიშალა");
+    },
+    onError: () => {
+      alert("Error removing order");
+    },
+  });
+
   return (
-    <button className="flex items-center gap-1 border border-gray-500 bg-gray-500 p-1 hover:bg-transparent text-white hover:text-gray-500">
+    <button
+      onClick={remove}
+      className="flex items-center gap-1 border border-gray-500 hover:border-darkRed bg-gray-500 p-1 hover:bg-darkRed text-white"
+    >
       <AiFillDelete />
-      <span>შეკვეთის გაუქმება</span>
+      <span>შეკვეთს გაუქმება</span>
     </button>
   );
 };
