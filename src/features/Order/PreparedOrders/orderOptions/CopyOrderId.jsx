2b9fa1c46a889a1db492fb92d90a8c7fa146d52c
Sandro <vipsandro11@gmail.com>
2023-09-18 12:45:13 +0400

    feat: Enhance component with copy-to-clipboard functionality

--- src/features/Order/PreparedOrders/orderOptions/CopyOrderId.jsx
+++ src/features/Order/PreparedOrders/orderOptions/CopyOrderId.jsx
@@ -1,9 +1,28 @@
+import React, { useState } from "react";
 import { AiFillCopy } from "react-icons/ai";
-const CopyOrderId = () => {
+
+const CopyOrderId = ({ orderId }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(orderId).then(() => {
+      setIsCopied(true);
+
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 3000);
+    });
+  };
+
   return (
-    <button className="flex items-center gap-1 border border-gray-500 bg-gray-500 p-1 hover:bg-transparent text-white hover:text-gray-500">
+    <button
+      onClick={handleCopyClick}
+      className={`flex items-center gap-1 border border-gray-500 bg-gray-500 p-1 hover:bg-transparent text-white hover:text-gray-500 ${
+        isCopied ? "bg-green-600 hover:text-green-600 border-green-600" : ""
+      }`}
+    >
       <AiFillCopy />
-      <span>აიდის დაკოპირება</span>
+      <span>{isCopied ? "აიდი დაკოპირდა" : "აიდის დაკოპირება"}</span>
     </button>
   );
 };
