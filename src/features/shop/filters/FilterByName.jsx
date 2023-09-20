c4c46824d24894a752f36a73b1d81046bb7448ab
Sandro <vipsandro11@gmail.com>
2023-09-20 15:33:30 +0400

    feat: product name filtering

---
+++ src/features/shop/filters/FilterByName.jsx
@@ -0,0 +1,8 @@
+
+const FilterByName = () => {
+  return (
+   <input type="text" placeholder="ძებნა" className="mb-5 border border-greyBorder p-1 rounded-md focus:outline-none" />
+  )
+}
+
+export default FilterByName
