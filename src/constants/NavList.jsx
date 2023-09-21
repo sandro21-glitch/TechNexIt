7c139e20ae816adaeeaef8bf53c6bc7cce3aad00
Sandro <vipsandro11@gmail.com>
2023-09-21 21:29:06 +0400

    refactor: Enhance Navigation Items with Paths

--- src/constants/NavList.jsx
+++ src/constants/NavList.jsx
@@ -2,30 +2,35 @@ import { AiTwotoneHome, AiFillShop, AiFillUnlock } from "react-icons/ai";
 import { BiLogoBlogger, BiGitCompare } from "react-icons/bi";
 import { FaUsersCog } from "react-icons/fa";
 
-export default[
+export default [
   {
     id: 1,
     icon: <AiTwotoneHome />,
     name: "მთავარი",
+    path: "/",
   },
   {
     id: 2,
     icon: <AiFillShop />,
     name: "მაღაზია",
+    path: "/shop",
   },
   {
     id: 3,
     icon: <BiLogoBlogger />,
     name: "ბლოგი",
+    path: "/",
   },
   {
     id: 4,
     icon: <FaUsersCog />,
     name: "სერვისები",
+    path: "/",
   },
   {
     id: 5,
     icon: <BiGitCompare />,
     name: "შედარება",
+    path: "/",
   },
 ];
