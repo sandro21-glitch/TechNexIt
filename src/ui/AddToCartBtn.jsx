8fc05e4b21db853e69aacc75f329a22e5516c1f8
Sandro <vipsandro11@gmail.com>
2023-09-21 16:04:24 +0400

    feat: Add shop-specific styling

--- src/ui/AddToCartBtn.jsx
+++ src/ui/AddToCartBtn.jsx
@@ -1,13 +1,23 @@
 import { BsCartPlus } from "react-icons/bs";
 import { useDispatch } from "react-redux";
 import { addItem } from "../features/cart/cartSlice";
-const AddToCartBtn = ({ product }) => {
+const AddToCartBtn = ({ product, shop = false }) => {
+
   const dispatch = useDispatch();
 
   const handleAddItem = () => {
     dispatch(addItem(product));
   };
-
+  if (shop) {
+    return (
+      <button
+        onClick={handleAddItem}
+        className="w-auto border p-2 text-white border-transparent bg-darkBlue hover:border-darkBlue hover:bg-transparent hover:text-darkBlue"
+      >
+        <BsCartPlus />
+      </button>
+    );
+  }
   return (
     <button
       onClick={handleAddItem}
