1ad51a3406bbc885fbe6ad585305147697ee7189
Sandro <vipsandro11@gmail.com>
2023-09-22 11:44:48 +0400

    feat: Add grid view toggle functionality and header filters

--- src/features/shop/ShopProducts.jsx
+++ src/features/shop/ShopProducts.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import SingleShopItem from "./SingleShopItem";
+import React, { useState } from "react";
 import { filterAllByCategory } from "../../services/apiProduct";
 import { useQuery } from "react-query";
 import PaginatedItems from "./PaginatedItems";
-
+import HeaderFilters from "./filters/header filters/HeaderFilters";
 const ShopProducts = () => {
+  const [isGrid, setIsGrid] = useState(true);
   const {
     data: allProduct,
     isLoading,
@@ -13,12 +13,21 @@ const ShopProducts = () => {
     queryFn: ["fetchProducts"],
     queryFn: () => filterAllByCategory(""),
   });
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading....</p>;
   if (isError) return <p>Error...</p>;
   return (
     <div className="flex-1">
+      <HeaderFilters
+        allProduct={allProduct}
+        setIsGrid={setIsGrid}
+        isGrid={isGrid}
+      />
       {/* Products grid */}
-      <PaginatedItems itemsPerPage={9} allProducts={allProduct} />
+      <PaginatedItems
+        itemsPerPage={9}
+        allProducts={allProduct}
+        isGrid={isGrid}
+      />
     </div>
   );
 };
