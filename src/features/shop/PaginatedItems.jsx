511f61617284713b5d62ae91d5852ee63bf095d1
Sandro <vipsandro11@gmail.com>
2023-09-23 15:47:06 +0400

    feat: Enhance PaginatedItems component to support grid and list view

--- src/features/shop/PaginatedItems.jsx
+++ src/features/shop/PaginatedItems.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleShopItem from "./SingleShopItem";
-import { useState } from "react";
 import ReactPaginate from "react-paginate";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
-const PaginatedItems = ({ itemsPerPage, allProducts, isGrid }) => {
-  const [itemOffset, setItemOffset] = useState(0);
-  const endOffset = itemOffset + itemsPerPage;
-  const currentProducts = allProducts.slice(itemOffset, endOffset);
+
+const PaginatedItems = ({
+  selectedCategory,
+  itemsPerPage,
+  allProducts,
+  isGrid,
+}) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const endOffset = (currentPage + 1) * itemsPerPage;
+  const startOffset = currentPage * itemsPerPage;
+  const currentProducts = allProducts.slice(startOffset, endOffset);
   const pageCount = Math.ceil(allProducts.length / itemsPerPage);
 
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % allProducts.length;
-    setItemOffset(newOffset);
+  const handlePageClick = (selected) => {
+    setCurrentPage(selected.selected);
   };
+
+  useEffect(() => {
+    // Reset currentPage to 0 when selectedCategory changes
+    setCurrentPage(0);
+  }, [selectedCategory]);
+
   return (
     <div>
       <ul
@@ -32,6 +43,7 @@ const PaginatedItems = ({ itemsPerPage, allProducts, isGrid }) => {
         onPageChange={handlePageClick}
         pageRangeDisplayed={5}
         pageCount={pageCount}
+        forcePage={currentPage} // Set the current page using forcePage
         previousLabel={<GrFormPrevious className="text-2xl" />}
         containerClassName="flex justify-start items-center font-rexFontLight"
         pageClassName="mx-2 w-7 h-7 border flex items-center justify-center"
