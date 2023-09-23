4aa52beb99441797f762f9b41794d6c46446fe70
Sandro <vipsandro11@gmail.com>
2023-09-23 15:47:40 +0400

    feat: Enhance ShopFilters component to include category selection and sorting options

--- src/features/shop/ShopFilters.jsx
+++ src/features/shop/ShopFilters.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import FilterByName from "./filters/FilterByName";
 import FilterByCategory from "./filters/FilterByCategory";
+import SortByPrice from "./filters/SortByPrice";
+import { useEffect } from "react";
 
-const ShopFilters = () => {
+const ShopFilters = ({
+  setSelectedCategory,
+  selectedCategory,
+  products,
+  setAllProductData,
+}) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    if (products && products.length > 0) {
+      const uniqueCategories = [
+        ...new Set(products.map((product) => product.category)),
+      ];
+      setCategories(uniqueCategories);
+    }
+  }, [products]);
   return (
-    <div>
-      <FilterByName />
-      <FilterByCategory />
+    <div className="">
+      <FilterByName
+        selectedCategory={selectedCategory}
+        products={products}
+        setAllProductData={setAllProductData}
+      />
+      <FilterByCategory
+        products={products}
+        categories={categories}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        setAllProductData={setAllProductData}
+      />
+      <p className="font-bold">Discount Filter</p>
+      <p className="font-bold">show only available products</p>
+      <SortByPrice />
     </div>
   );
 };
