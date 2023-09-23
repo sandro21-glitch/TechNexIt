d8df57661164e4ae0cac67648158788aa33d70ac
Sandro <vipsandro11@gmail.com>
2023-09-23 15:50:45 +0400

    feat: Enhance HeaderFilters component to include ItemsPerPage control

--- src/features/shop/filters/header filters/HeaderFilters.jsx
+++ src/features/shop/filters/header filters/HeaderFilters.jsx
@@ -1,12 +1,23 @@
 import GridControl from "./GridControl";
+import ItemsPerPage from "./ItemsPerPage";
 import ProductLength from "./ProductLength";
 
-const HeaderFilters = ({ setIsGrid, isGrid, allProduct }) => {
+const HeaderFilters = ({
+  setIsGrid,
+  isGrid,
+  allProduct,
+  setItemsPerPage,
+  itemsPerPage,
+}) => {
   return (
-    <div className="font-rexFontLight mb-5 grid grid-cols-1 md:grid-cols-[auto,auto,1fr] items-center gap-5">
+    <div className="font-rexFontLight mb-5 grid grid-cols-1 md:grid-cols-[auto,auto,1fr,auto] items-center gap-5">
       <GridControl isGrid={isGrid} setIsGrid={setIsGrid} />
       <ProductLength allProduct={allProduct} />
       <hr />
+      <ItemsPerPage
+        itemsPerPage={itemsPerPage}
+        setItemsPerPage={setItemsPerPage}
+      />
     </div>
   );
 };
