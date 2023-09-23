bd8636c5fbf4158c259244bfe27d7d87b5acbc5e
Sandro <vipsandro11@gmail.com>
2023-09-23 15:50:16 +0400

    refactor: Update FilterByName component for dynamic search filtering

--- src/features/shop/filters/FilterByName.jsx
+++ src/features/shop/filters/FilterByName.jsx
@@ -1,8 +1,43 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
-const FilterByName = () => {
+const FilterByName = ({
+  setAllProductData,
+  products,
+  selectedCategory,
+}) => {
+  const [searchProduct, setSearchProduct] = useState("");
+  useEffect(() => {
+    if (selectedCategory === "") {
+      if (searchProduct !== "") {
+        const newProducts = products.filter((product) =>
+          product.name.toLowerCase().includes(searchProduct.toLowerCase())
+        );
+        setAllProductData(newProducts);
+      } else {
+        setAllProductData(products);
+      }
+    } else {
+      const categoryAndNameProducts = products.filter(
+        (product) =>
+          product.category.toLowerCase() === selectedCategory.toLowerCase() &&
+          product.name.toLowerCase().includes(searchProduct.toLowerCase())
+      );
+      setAllProductData(categoryAndNameProducts);
+    }
+  }, [searchProduct, selectedCategory, products]);
+
   return (
-   <input type="text" placeholder="ძებნა" className="mb-5 border border-greyBorder p-1 rounded-md focus:outline-none" />
-  )
-}
+    <input
+      type="text"
+      placeholder="ძებნა"
+      onChange={(e) => setSearchProduct(e.target.value)}
+      value={searchProduct}
+      className="font-rexFontLight mb-5 w-full p-2 rounded-md
+       border border-greyBorder focus:border-gray-600
+        transition-all ease-in duration-200 outline-none"
+    />
+  );
+};
 
-export default FilterByName
+export default FilterByName;
