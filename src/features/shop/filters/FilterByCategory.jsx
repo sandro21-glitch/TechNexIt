20f6909b23f2cda5897b8f076db3947ead094b89
Sandro <vipsandro11@gmail.com>
2023-09-23 15:49:50 +0400

    refactor: Update FilterByCategory component to use dynamic category filtering

--- src/features/shop/filters/FilterByCategory.jsx
+++ src/features/shop/filters/FilterByCategory.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 
-const FilterByCategory = () => {
+const FilterByCategory = ({
+  categories,
+  setSelectedCategory,
+  setAllProductData,
+  products,
+  selectedCategory,
+}) => {
+  const handleFilterData = (category) => {
+    setSelectedCategory(category);
+    const filteredData = products.filter((item) => {
+      return item.category === category;
+    });
+    setAllProductData(filteredData);
+  };
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 font-rexFontLight">
       <div>
-        <h6 className="font-rexFontLight font-semibold">
-          კომპიუტერის ნაწილები
-        </h6>
-        <ul className="font-rexFontBold">
-          <li className="cursor-pointer">პროცესორები</li>
-          <li className="cursor-pointer">ვიდეო ბარათები</li>
-          <li className="cursor-pointer">ოპერატიულები</li>
-          <li className="cursor-pointer">SSD მეხსიერება</li>
-          <li className="cursor-pointer">დედაპლატები</li>
+        <h6 className="text-[1.1rem] font-bold">კომპიუტერის ნაწილები</h6>
+        <ul>
+          {categories.map((category, index) => {
+            return (
+              <li
+                onClick={() => handleFilterData(category)}
+                key={index}
+                className={`${
+                  selectedCategory === category
+                    ? "text-darkBlue font-bold"
+                    : "text-black"
+                } cursor-pointer`}
+              >
+                {category}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
