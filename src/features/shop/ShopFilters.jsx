61b8f21e8dfbc4f9b742dcd50fd6b52461613476
Sandro <vipsandro11@gmail.com>
2023-09-26 14:46:23 +0400

    feat: Add FilterByPrice component to ShopFilters

--- src/features/shop/ShopFilters.jsx
+++ src/features/shop/ShopFilters.jsx
@@ -3,6 +3,7 @@ import FilterByName from "./filters/FilterByName";
 import FilterByCategory from "./filters/FilterByCategory";
 import { useEffect } from "react";
 import StoreType from "./filters/StoreType";
+import FilterByPrice from "./filters/FilterByPrice";
 
 const ShopFilters = ({
   allProductData,
@@ -23,28 +24,36 @@ const ShopFilters = ({
     }
   }, [products]);
   return (
-    <div>
-      <FilterByName
-        filterType={filterType}
-        selectedCategory={selectedCategory}
-        products={products}
-        setAllProductData={setAllProductData}
-      />
-      <FilterByCategory
-        filterType={filterType}
-        products={products}
-        categories={categories}
-        selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}
-        setAllProductData={setAllProductData}
-      />
-      <StoreType
-        filterType={filterType}
-        setFilterType={setFilterType}
-        selectedCategory={selectedCategory}
-        products={products}
-        setAllProductData={setAllProductData}
-      />
+    <div className="relative">
+      <div className="md:sticky md:top-[1rem]">
+        <FilterByName
+          filterType={filterType}
+          selectedCategory={selectedCategory}
+          products={products}
+          setAllProductData={setAllProductData}
+        />
+        <FilterByCategory
+          filterType={filterType}
+          products={products}
+          categories={categories}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          setAllProductData={setAllProductData}
+        />
+        <StoreType
+          filterType={filterType}
+          setFilterType={setFilterType}
+          selectedCategory={selectedCategory}
+          products={products}
+          setAllProductData={setAllProductData}
+        />
+        <FilterByPrice
+          filterType={filterType}
+          selectedCategory={selectedCategory}
+          products={products}
+          setAllProductData={setAllProductData}
+        />
+      </div>
     </div>
   );
 };
