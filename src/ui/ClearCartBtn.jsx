0cfcd6bdffc37e7cbfc656e3fc9a80b737580a33
Sandro <vipsandro11@gmail.com>
2023-09-26 14:48:00 +0400

    refactor: Enhance ClearCartBtn component with toast notification

--- src/ui/ClearCartBtn.jsx
+++ src/ui/ClearCartBtn.jsx
@@ -1,10 +1,15 @@
 import { useDispatch } from "react-redux";
 import { clearCart } from "../features/cart/cartSlice";
+import toast from "react-hot-toast";
 const ClearCartBtn = () => {
   const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+    toast.success("თქვენი კალათა გასუფთავდა");
+  };
   return (
     <button
-      onClick={() => dispatch(clearCart())}
+      onClick={handleClearCart}
       className="
       border border-gray-700 bg-gray-700 text-white 
       px-2 py-1 font-rexFontLight hover:bg-transparent hover:text-black 
