ea02a0e1b41961ac3e549865b78e18d55334c625
Sandro <vipsandro11@gmail.com>
2023-09-28 13:18:40 +0400

    style: Adjust SmallSpinner size in SmallSpinner component

--- src/ui/SmallSpinner.jsx
+++ src/ui/SmallSpinner.jsx
@@ -1,9 +1,9 @@
 const SmallSpinner = () => {
   return (
-    <div role="status" className="flex items-center justify-center ">
+    <div role="status" className="flex items-center justify-center">
       <svg
         aria-hidden="true"
-        className="inline w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="inline w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
