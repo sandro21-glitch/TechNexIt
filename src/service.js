a98d96939d8157045bfd93a9e4951ac07443733c
Sandro <vipsandro11@gmail.com>
2023-09-28 21:26:10 +0400

    chore: Update Supabase Environment Variable Prefix

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = import.meta.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = import.meta.env.REACT_APP_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_REACT_APP_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
