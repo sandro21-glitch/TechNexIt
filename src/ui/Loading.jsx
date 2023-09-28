d3db11410ae64bfa386cfc89b70c43102be3cfdf
Sandro <vipsandro11@gmail.com>
2023-09-28 13:18:26 +0400

    style: Adjust Loading component layout for better alignment

--- src/ui/Loading.jsx
+++ src/ui/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center min-h-[50vh]">
       <div className="lds-ellipsis">
         <div></div>
         <div></div>
