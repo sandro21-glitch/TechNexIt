c719ddd7193d33f51e44d5ab54e82b2eac6032c4
Sandro <vipsandro11@gmail.com>
2023-09-30 21:42:47 +0400

    style: update AmountCheck component styling and cursor behavior

--- src/ui/AmountCheck.jsx
+++ src/ui/AmountCheck.jsx
@@ -1,24 +1,23 @@
 import { FaCubes } from "react-icons/fa";
 
 const AmountCheck = ({ amount }) => {
-  
   if (amount < 1) {
     return (
-      <p className="p-2 flex items-center gap-2 bg-darkRed w-fit mb-5 text-white">
+      <div className="cursor-default p-2 flex items-center gap-2 bg-darkRed w-fit mb-5 text-white">
         <span>
           <FaCubes />
         </span>
         <span>წინასწარი შეკვეთით</span>
-      </p>
+      </div>
     );
   }
   return (
-    <p className="p-2 flex items-center gap-2 bg-lightBlue w-fit mb-5 text-white">
+    <div className="cursor-default p-2 flex items-center gap-2 bg-lightBlue w-fit mb-5 text-white">
       <span>
         <FaCubes />
       </span>
       <span>მარაგშია</span>
-    </p>
+    </div>
   );
 };
 
