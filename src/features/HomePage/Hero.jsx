a7f8757796914800de2a332988283f6c3ab88312
Sandro <vipsandro11@gmail.com>
2023-09-30 21:41:06 +0400

    feat: Update Hero component with new images and logo

--- src/features/HomePage/Hero.jsx
+++ src/features/HomePage/Hero.jsx
@@ -1,5 +1,6 @@
 import heroOne from "/images/hero-1.png";
 import heroTwo from "/images/hero-2.png";
+import logo from "/images/logo-no-background.png";
 
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 // or other themes
@@ -20,21 +21,25 @@ const Hero = () => {
         }}
       >
         <SplideSlide>
-          <img
-            src={heroOne}
-            alt="Image 1"
-            className="w-full h-auto max-h-[50vh] object-cover"
-          />
+          <div>
+            <img
+              src={heroOne}
+              alt="Image 1"
+              className="relative w-full h-[30vh] md:h-[50vh] object-cover"
+            />
+            <span className="absolute top-5 right-5">
+              <img src={logo} alt="" className="w-32 md:w-52" />
+            </span>
+          </div>
         </SplideSlide>
-
         <SplideSlide>
           <img
             src={heroTwo}
             alt="Image 2"
-            className="w-full h-auto max-h-[50vh] object-cover"
+            className="w-full h-[30vh] md:h-[50vh] object-cover"
           />
         </SplideSlide>
-        <div className="splide__progress mt-[15rem] lg:mt-[30rem]">
+        <div className="splide__progress mt-[15rem]">
           <div className="splide__progress__bar" />
         </div>
       </Splide>
